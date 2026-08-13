48811b1850446c6b5b603497efacfc8576a0351d
pravie7 <praveenmdu127@outlook.com>
2026-08-13 21:00:42 +0530

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header({ title }) {
+  return (
+    <div className="header">
+      <div>
+        <h1>{title}</h1>
+        <p>Private Personal Finance Dashboard</p>
+      </div>
+    </div>
+  );
+}
