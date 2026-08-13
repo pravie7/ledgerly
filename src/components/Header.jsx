a1c1b277890b011911c232c98e3b5592fe425886
pravie7 <praveenmdu127@outlook.com>
2026-08-13 21:10:32 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,8 @@
 export default function Header({ title }) {
   return (
     <div className="header">
-      <div>
-        <h1>{title}</h1>
-        <p>Private Personal Finance Dashboard</p>
-      </div>
+      <h1>{title}</h1>
+      <p>Private Personal Finance Dashboard</p>
     </div>
   );
 }
