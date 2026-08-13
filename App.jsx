d05d7096c756c169b32ee39eeb8da018331b8771
pravie7 <praveenmdu127@outlook.com>
2026-08-13 20:30:20 +0530

    Delete App.jsx

--- App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App(){
-  return (
-    <div style={{fontFamily:'Arial',padding:40}}>
-      <h1>Ledgerly</h1>
-      <p>Your finance app starter is working.</p>
-    </div>
-  )
-}
