ab0e5ff3034f8cdc9d44e68cf527c0a68f4ff938
pravie7 <praveenmdu127@outlook.com>
2026-08-14 20:04:21 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,28 @@
-const BASE = "";
+const BASE =
+  "https://ledgerly-api.praveenmdu127.workers.dev";
 
 export async function getTransactions() {
   const res = await fetch(`${BASE}/api/transactions`);
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch transactions");
+  }
+
   return await res.json();
 }
 
-export async function addTransaction(tx) {
-  await fetch(`${BASE}/api/transactions`, {
+export async function addTransaction(transaction) {
+  const res = await fetch(`${BASE}/api/transactions`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(tx),
+    body: JSON.stringify(transaction),
   });
+
+  if (!res.ok) {
+    throw new Error("Failed to save transaction");
+  }
+
+  return await res.json();
 }
